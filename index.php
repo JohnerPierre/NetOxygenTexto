437e49fa998e9901c6970b28e8e264f704bf84bc
JohnerPierre <pierre.johner@gmail.com>
2017-10-10 14:09:57 +0200

    Connection To The Service

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-echo "helloWorld";
+<?php
+require_once(dirname(__FILE__) . '/No2SMS_Client.class.php');
+//cG9vcmx5Y29kZWRwYXNzd29yZA==
+//+41 (076) 536 37 76
+
+
+$user        = "devjob";
+$password    = "cG9vcmx5Y29kZWRwYXNzd29yZA==";
+//$destination = $argv[3];
+$message     = "Pierre Johner";
+
+
+$client = new No2SMS_Client($user, base64_decode($password));
+
+var_dump($client);
+
+if (!$client->auth())
+        die('mauvais utilisateur ou mot de passe');
+
+?>
