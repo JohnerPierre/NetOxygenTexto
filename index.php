ce7633bd374507a929d1d61c329e0540913d5818
JohnerPierre <pierre.johner@gmail.com>
2017-10-10 14:24:48 +0200

    Add Securtity

--- index.php
+++ index.php
@@ -6,15 +6,27 @@ require_once(dirname(__FILE__) . '/No2SMS_Client.class.php');
 
 $user        = "devjob";
 $password    = "cG9vcmx5Y29kZWRwYXNzd29yZA==";
-//$destination = $argv[3];
-$message     = "Pierre Johner";
+$destination = "0041766164122";
+$message = "Pierre Johner";
+
+
 
 
 $client = new No2SMS_Client($user, base64_decode($password));
 
 var_dump($client);
 
-if (!$client->auth())
-        die('mauvais utilisateur ou mot de passe');
+try {
+    if (!$client->auth())
+            die('Bad Login, Password');
+
+    $res = $client->send_message($destination, $message);
+
+    var_dump($res);
+    
+} catch (No2SMS_Exception $e) {
+    printf("Connection problem: %s", $e->getMessage());
+    exit(1);
+}
 
 ?>
